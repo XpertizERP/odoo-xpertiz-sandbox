78c6bbebc2a0db662b79d5018b24b365f4ba3fbd
francois.lange@xpertiz.lu <francois.lange@xpertiz.lu>
2019-03-25 16:56:04 +0100

    test controller

--- test_web/controllers/controllers.py
+++ test_web/controllers/controllers.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
- class testController(http.Controller):
-     @http.route('/test', auth='public')
-     def index(self, **kw):
-         return "Hello, world"
+class testController(http.Controller):
+    @http.route('/test', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
 #     @http.route('/openacademy/openacademy/objects/', auth='public')
 #     def list(self, **kw):
